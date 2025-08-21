e3173eb7778d5f24e1ea344ed6944922ab5f7dd4
Unais Shaikh <unais.shaikh.dev@gmail.com>
2025-08-21 17:12:39 +0530

    text change in dashboard card

--- client/src/features/admin/dashboard/Overview/components/OverviewCards.jsx
+++ client/src/features/admin/dashboard/Overview/components/OverviewCards.jsx
@@ -9,7 +9,7 @@ const OverviewCards = ({ blogs, sponsorRequests, sponsors }) => {
     <>
       <Card>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">Total Posts</CardTitle>
+          <CardTitle className="text-sm font-medium">Total Blog Posts</CardTitle>
           <FileText className="h-4 w-4 text-muted-foreground" />
         </CardHeader>
         <CardContent>
@@ -20,7 +20,7 @@ const OverviewCards = ({ blogs, sponsorRequests, sponsors }) => {
 
       <Card>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">Pending Requests</CardTitle>
+          <CardTitle className="text-sm font-medium">Sponsors Pending Requests</CardTitle>
           <HandHeart className="h-4 w-4 text-muted-foreground" />
         </CardHeader>
         <CardContent>
